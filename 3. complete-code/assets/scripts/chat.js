963ec5ac335c450561866e2ec5a05ec23b6c487d
itswisdomagain <wisdom.arerosuoghene@gmail.com>
2017-10-02 11:02:28 +0100

    added chat functionality to complete project

--- 3. complete-code/assets/scripts/chat.js
+++ 3. complete-code/assets/scripts/chat.js
@@ -6,9 +6,46 @@ var unsubscribe = firebase.auth().onAuthStateChanged(function(user) {
     // navigate the user to the chat page
     window.location.href = 'index.html';
   }
+  else {
+    // the user us logged in, start listening to messages in the appropriate node of our database
+    startListeningToChats();
+  }
 
   unsubscribe();
 });
+
+// this holds the reference to the chat node of our Firebase database
+function getDatabaseReference() {
+  // Get a reference to the database service
+  var database = firebase.database();
+  // this is a reference to the chat node of our Firebase database
+  var chatNode = firebase.database().ref().child('chat');
+
+  return chatNode;
+}
+
+//this next funtion listens for changes to the chat node of the database
+function startListeningToChats(){
+  // get a reference to the chat node that holds all chats
+  var chatNode = getDatabaseReference();
+
+  // whenever a child node is added to the parent node of all chats, call the newMessageReceived fucnction
+  chatNode.on('child_added', newMessageRecieved);
+}
+
+function newMessageRecieved(messageNode) {
+  // create a list item element to add to the chat messages unordered list
+  var newMessageListItem = document.createElement('li');
+
+  // the actual message received is in messageNode.val()
+  newMessageListItem.innerHTML = messageNode.val();
+
+  // append the newly created list item to the unordered list and voila! the message should appear
+  document.getElementById('chat-messages').appendChild(newMessageListItem);
+
+  //scroll to bottom so the newly added message is visible
+  document.getElementById('chat-messages').scrollTop = document.getElementById('chat-messages').scrollHeight;
+}
 
 function sendMessage() {
   // get reference to the message input box
@@ -21,7 +58,24 @@ function sendMessage() {
     alert("You haven't entered a message to send...");
   }
   else {
-    alert("To enable message sending capabilities using Firebase Realtime Database, refer to the Firebase Database slide");
+    // get a reference to the chat node that holds all chats
+    var chatNode = getDatabaseReference();
+
+    // save message to the chat node
+    chatNode.push(message)
+    .then(function(result){
+      // this function will be called if your message was successfully sent
+      // do whatever you want to do here
+    })
+    .catch(function(error){
+      // message was not sent, an error occured
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      alert(errorMessage);
+    });
+
+    // clear the message box to allow further messaging
+    messageInputBox.value = '';
   }
 }
 
